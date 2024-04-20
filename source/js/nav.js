e242705525522ed6e0a645c4b0afcb2126c4d7af
Юлия Агафонова <yuliya-agafonova-1997@mail.ru>
2024-04-20 21:19:26 +0300

    hero item 1

--- source/js/nav.js
+++ source/js/nav.js
@@ -4,5 +4,10 @@ const buttonIcon = document.querySelector('.header__button-icon');
 
 buttonNav.onclick = function () {
   nav.classList.toggle('active');
-  buttonIcon.src = '/__spritemap#sprite-logo-header';
-}
+  if (buttonIcon.href = '/__spritemap#sprite-menu') {
+    buttonIcon.href = '/__spritemap#sprite-cross';
+  } else {
+    buttonIcon.href = '/__spritemap#sprite-menu';
+  }
+};
+// оверлей на все + меняется кнопка + открывается меню все в одной функции
